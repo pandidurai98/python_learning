e98b0ae9c36d0ff4f267609e0e31635010893a92
pandidurai98 <pandianps10@gmail.com>
2025-04-01 14:33:25 +0530

    Added addition functionality

--- add.py
+++ add.py
@@ -1,1 +1,3 @@
+from add_fun import function_to_pass_var
 print("hello git")
+function_to_pass_var(4, 7)
---
+++ add_fun.py
@@ -0,0 +1,5 @@
+
+def function_to_pass_var(var1, var2):
+    print("function")
+    result = var1 + var2
+    print('result values =',result)
